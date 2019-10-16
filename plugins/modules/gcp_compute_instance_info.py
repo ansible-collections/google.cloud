10ef9cc739894fb26872664d1e9bdf17dfda0409
Alex Stephen <alexstephen@google.com>
2019-10-16 17:54:28 +0000

    [Ansible] Stop building deprecated facts modules in collections
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- plugins/modules/gcp_compute_instance_info.py
+++ plugins/modules/gcp_compute_instance_info.py
@@ -32,8 +32,6 @@ DOCUMENTATION = '''
 module: gcp_compute_instance_info
 description:
 - Gather info for GCP Instance
-- This module was called C(gcp_compute_instance_facts) before Ansible 2.9. The usage
-  has not changed.
 short_description: Gather info for GCP Instance
 version_added: '2.7'
 author: Google Inc. (@googlecloudplatform)
@@ -570,9 +568,6 @@ import json
 
 def main():
     module = GcpModule(argument_spec=dict(filters=dict(type='list', elements='str'), zone=dict(required=True, type='str')))
-
-    if module._name == 'gcp_compute_instance_facts':
-        module.deprecate("The 'gcp_compute_instance_facts' module has been renamed to 'gcp_compute_instance_info'", version='2.13')
 
     if not module.params['scopes']:
         module.params['scopes'] = ['https://www.googleapis.com/auth/compute']
