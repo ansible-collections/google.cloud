d0f02efb876718618849a41a3d365eefb5bc8db4
Chris Stephens <chrisst@google.com>
2019-11-19 19:12:30 +0000

    Refactor async class to support kubernetes objects
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- plugins/modules/gcp_cloudbuild_trigger.py
+++ plugins/modules/gcp_cloudbuild_trigger.py
@@ -109,7 +109,7 @@ options:
     - Branch and tag names in trigger templates are interpreted as regular expressions.
       Any branch or tag change that matches that regular expression will trigger a
       build.
-    required: false
+    required: true
     type: dict
     suboptions:
       project_id:
@@ -648,6 +648,7 @@ def main():
             ignored_files=dict(type='list', elements='str'),
             included_files=dict(type='list', elements='str'),
             trigger_template=dict(
+                required=True,
                 type='dict',
                 options=dict(
                     project_id=dict(type='str'),
