5aa73d0bee5c26b9f42934b8d4d59f2379970a95
Modular Magician <magic-modules@google.com>
2020-02-20 00:39:26 +0000

    document updated with right API url for resource google_compute_forwarding_rule. Issue-#5598 (#3098)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- plugins/modules/gcp_compute_forwarding_rule.py
+++ plugins/modules/gcp_compute_forwarding_rule.py
@@ -254,7 +254,7 @@ options:
     - This only alters the User Agent string for any API requests.
     type: str
 notes:
-- 'API Reference: U(https://cloud.google.com/compute/docs/reference/v1/forwardingRule)'
+- 'API Reference: U(https://cloud.google.com/compute/docs/reference/v1/forwardingRules)'
 - 'Official Documentation: U(https://cloud.google.com/compute/docs/load-balancing/network/forwarding-rules)'
 - for authentication, you can set service_account_file using the C(gcp_service_account_file)
   env variable.
