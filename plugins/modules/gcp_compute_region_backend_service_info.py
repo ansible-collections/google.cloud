0db243fdd0c62ab53c73dd94e587cf94c8ba6003
Modular Magician <magic-modules@google.com>
2020-04-29 20:31:39 +0000

    Promote region* compute services to GA (#3381)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- plugins/modules/gcp_compute_region_backend_service_info.py
+++ plugins/modules/gcp_compute_region_backend_service_info.py
@@ -118,6 +118,15 @@ resources:
   returned: always
   type: complex
   contains:
+    affinityCookieTtlSec:
+      description:
+      - Lifetime of cookies in seconds if session_affinity is GENERATED_COOKIE. If
+        set to 0, the cookie is non-persistent and lasts only until the end of the
+        browser session (or equivalent). The maximum allowed value for TTL is one
+        day.
+      - When the load balancing scheme is INTERNAL, this field is not used.
+      returned: success
+      type: int
     backends:
       description:
       - The set of backends that serve this RegionBackendService.
@@ -146,6 +155,12 @@ resources:
           - Provide this property when you create the resource.
           returned: success
           type: str
+        failover:
+          description:
+          - This field designates whether this is a failover backend. More than one
+            failover backend can be configured for a given RegionBackendService.
+          returned: success
+          type: bool
         group:
           description:
           - The fully-qualified URL of an Instance Group or Network Endpoint Group
@@ -224,6 +239,113 @@ resources:
           - Cannot be set for INTERNAL backend services.
           returned: success
           type: str
+    circuitBreakers:
+      description:
+      - Settings controlling the volume of connections to a backend service. This
+        field is applicable only when the `load_balancing_scheme` is set to INTERNAL_MANAGED
+        and the `protocol` is set to HTTP, HTTPS, or HTTP2.
+      returned: success
+      type: complex
+      contains:
+        maxRequestsPerConnection:
+          description:
+          - Maximum requests for a single backend connection. This parameter is respected
+            by both the HTTP/1.1 and HTTP/2 implementations. If not specified, there
+            is no limit. Setting this parameter to 1 will effectively disable keep
+            alive.
+          returned: success
+          type: int
+        maxConnections:
+          description:
+          - The maximum number of connections to the backend cluster.
+          - Defaults to 1024.
+          returned: success
+          type: int
+        maxPendingRequests:
+          description:
+          - The maximum number of pending requests to the backend cluster.
+          - Defaults to 1024.
+          returned: success
+          type: int
+        maxRequests:
+          description:
+          - The maximum number of parallel requests to the backend cluster.
+          - Defaults to 1024.
+          returned: success
+          type: int
+        maxRetries:
+          description:
+          - The maximum number of parallel retries to the backend cluster.
+          - Defaults to 3.
+          returned: success
+          type: int
+    consistentHash:
+      description:
+      - Consistent Hash-based load balancing can be used to provide soft session affinity
+        based on HTTP headers, cookies or other properties. This load balancing policy
+        is applicable only for HTTP connections. The affinity to a particular destination
+        host will be lost when one or more hosts are added/removed from the destination
+        service. This field specifies parameters that control consistent hashing.
+      - This field only applies when all of the following are true - * `load_balancing_scheme`
+        is set to INTERNAL_MANAGED * `protocol` is set to HTTP, HTTPS, or HTTP2 *
+        `locality_lb_policy` is set to MAGLEV or RING_HASH .
+      returned: success
+      type: complex
+      contains:
+        httpCookie:
+          description:
+          - Hash is based on HTTP Cookie. This field describes a HTTP cookie that
+            will be used as the hash key for the consistent hash load balancer. If
+            the cookie is not present, it will be generated.
+          - This field is applicable if the sessionAffinity is set to HTTP_COOKIE.
+          returned: success
+          type: complex
+          contains:
+            ttl:
+              description:
+              - Lifetime of the cookie.
+              returned: success
+              type: complex
+              contains:
+                seconds:
+                  description:
+                  - Span of time at a resolution of a second.
+                  - Must be from 0 to 315,576,000,000 inclusive.
+                  returned: success
+                  type: int
+                nanos:
+                  description:
+                  - Span of time that's a fraction of a second at nanosecond resolution.
+                    Durations less than one second are represented with a 0 seconds
+                    field and a positive nanos field. Must be from 0 to 999,999,999
+                    inclusive.
+                  returned: success
+                  type: int
+            name:
+              description:
+              - Name of the cookie.
+              returned: success
+              type: str
+            path:
+              description:
+              - Path to set for the cookie.
+              returned: success
+              type: str
+        httpHeaderName:
+          description:
+          - The hash based on the value of the specified header field.
+          - This field is applicable if the sessionAffinity is set to HEADER_FIELD.
+          returned: success
+          type: str
+        minimumRingSize:
+          description:
+          - The minimum number of virtual nodes to use for the hash ring.
+          - Larger ring sizes result in more granular load distributions. If the number
+            of hosts in the load balancing pool is larger than the ring size, each
+            host will be assigned a single virtual node.
+          - Defaults to 1024.
+          returned: success
+          type: int
     connectionDraining:
       description:
       - Settings for connection draining .
@@ -246,6 +368,44 @@ resources:
       - An optional description of this resource.
       returned: success
       type: str
+    failoverPolicy:
+      description:
+      - Policy for failovers.
+      returned: success
+      type: complex
+      contains:
+        disableConnectionDrainOnFailover:
+          description:
+          - 'On failover or failback, this field indicates whether connection drain
+            will be honored. Setting this to true has the following effect: connections
+            to the old active pool are not drained. Connections to the new active
+            pool use the timeout of 10 min (currently fixed). Setting to false has
+            the following effect: both old and new connections will have a drain timeout
+            of 10 min.'
+          - This can be set to true only if the protocol is TCP.
+          - The default is false.
+          returned: success
+          type: bool
+        dropTrafficIfUnhealthy:
+          description:
+          - This option is used only when no healthy VMs are detected in the primary
+            and backup instance groups. When set to true, traffic is dropped. When
+            set to false, new connections are sent across all VMs in the primary group.
+          - The default is false.
+          returned: success
+          type: bool
+        failoverRatio:
+          description:
+          - The value of the field must be in [0, 1]. If the ratio of the healthy
+            VMs in the primary backend is at or below this number, traffic arriving
+            at the load-balanced IP will be directed to the failover backend.
+          - In case where 'failoverRatio' is not set or all the VMs in the backup
+            backend are unhealthy, the traffic will be directed back to the primary
+            backend in the "force" mode, where traffic will be spread to the healthy
+            VMs with the best effort, or to all VMs when no VM is healthy.
+          - This field is only used with l4 load balancing.
+          returned: success
+          type: str
     fingerprint:
       description:
       - Fingerprint of this resource. A hash of the contents stored in this object.
@@ -271,6 +431,28 @@ resources:
         be used with the other(s).
       returned: success
       type: str
+    localityLbPolicy:
+      description:
+      - The load balancing algorithm used within the scope of the locality.
+      - The possible values are - ROUND_ROBIN - This is a simple policy in which each
+        healthy backend is selected in round robin order.
+      - LEAST_REQUEST - An O(1) algorithm which selects two random healthy hosts and
+        picks the host which has fewer active requests.
+      - RING_HASH - The ring/modulo hash load balancer implements consistent hashing
+        to backends. The algorithm has the property that the addition/removal of a
+        host from a set of N hosts only affects 1/N of the requests.
+      - RANDOM - The load balancer selects a random healthy host.
+      - ORIGINAL_DESTINATION - Backend host is selected based on the client connection
+        metadata, i.e., connections are opened to the same address as the destination
+        address of the incoming connection before the connection was redirected to
+        the load balancer.
+      - MAGLEV - used as a drop in replacement for the ring hash load balancer.
+      - Maglev is not as stable as ring hash but has faster table lookup build times
+        and host selection times. For more information about Maglev, refer to https://ai.google/research/pubs/pub44824
+        This field is applicable only when the `load_balancing_scheme` is set to INTERNAL_MANAGED
+        and the `protocol` is set to HTTP, HTTPS, or HTTP2.
+      returned: success
+      type: str
     name:
       description:
       - Name of the resource. Provided by the client when the resource is created.
@@ -281,6 +463,127 @@ resources:
         which cannot be a dash.
       returned: success
       type: str
+    outlierDetection:
+      description:
+      - Settings controlling eviction of unhealthy hosts from the load balancing pool.
+      - This field is applicable only when the `load_balancing_scheme` is set to INTERNAL_MANAGED
+        and the `protocol` is set to HTTP, HTTPS, or HTTP2.
+      returned: success
+      type: complex
+      contains:
+        baseEjectionTime:
+          description:
+          - The base time that a host is ejected for. The real time is equal to the
+            base time multiplied by the number of times the host has been ejected.
+            Defaults to 30000ms or 30s.
+          returned: success
+          type: complex
+          contains:
+            seconds:
+              description:
+              - Span of time at a resolution of a second. Must be from 0 to 315,576,000,000
+                inclusive.
+              returned: success
+              type: int
+            nanos:
+              description:
+              - Span of time that's a fraction of a second at nanosecond resolution.
+                Durations less than one second are represented with a 0 `seconds`
+                field and a positive `nanos` field. Must be from 0 to 999,999,999
+                inclusive.
+              returned: success
+              type: int
+        consecutiveErrors:
+          description:
+          - Number of errors before a host is ejected from the connection pool. When
+            the backend host is accessed over HTTP, a 5xx return code qualifies as
+            an error.
+          - Defaults to 5.
+          returned: success
+          type: int
+        consecutiveGatewayFailure:
+          description:
+          - The number of consecutive gateway failures (502, 503, 504 status or connection
+            errors that are mapped to one of those status codes) before a consecutive
+            gateway failure ejection occurs. Defaults to 5.
+          returned: success
+          type: int
+        enforcingConsecutiveErrors:
+          description:
+          - The percentage chance that a host will be actually ejected when an outlier
+            status is detected through consecutive 5xx. This setting can be used to
+            disable ejection or to ramp it up slowly. Defaults to 100.
+          returned: success
+          type: int
+        enforcingConsecutiveGatewayFailure:
+          description:
+          - The percentage chance that a host will be actually ejected when an outlier
+            status is detected through consecutive gateway failures. This setting
+            can be used to disable ejection or to ramp it up slowly. Defaults to 0.
+          returned: success
+          type: int
+        enforcingSuccessRate:
+          description:
+          - The percentage chance that a host will be actually ejected when an outlier
+            status is detected through success rate statistics. This setting can be
+            used to disable ejection or to ramp it up slowly. Defaults to 100.
+          returned: success
+          type: int
+        interval:
+          description:
+          - Time interval between ejection sweep analysis. This can result in both
+            new ejections as well as hosts being returned to service. Defaults to
+            10 seconds.
+          returned: success
+          type: complex
+          contains:
+            seconds:
+              description:
+              - Span of time at a resolution of a second. Must be from 0 to 315,576,000,000
+                inclusive.
+              returned: success
+              type: int
+            nanos:
+              description:
+              - Span of time that's a fraction of a second at nanosecond resolution.
+                Durations less than one second are represented with a 0 `seconds`
+                field and a positive `nanos` field. Must be from 0 to 999,999,999
+                inclusive.
+              returned: success
+              type: int
+        maxEjectionPercent:
+          description:
+          - Maximum percentage of hosts in the load balancing pool for the backend
+            service that can be ejected. Defaults to 10%.
+          returned: success
+          type: int
+        successRateMinimumHosts:
+          description:
+          - The number of hosts in a cluster that must have enough request volume
+            to detect success rate outliers. If the number of hosts is less than this
+            setting, outlier detection via success rate statistics is not performed
+            for any host in the cluster. Defaults to 5.
+          returned: success
+          type: int
+        successRateRequestVolume:
+          description:
+          - The minimum number of total requests that must be collected in one interval
+            (as defined by the interval duration above) to include this host in success
+            rate based outlier detection. If the volume is lower than this setting,
+            outlier detection via success rate statistics is not performed for that
+            host. Defaults to 100.
+          returned: success
+          type: int
+        successRateStdevFactor:
+          description:
+          - 'This factor is used to determine the ejection threshold for success rate
+            outlier ejection. The ejection threshold is the difference between the
+            mean success rate, and the product of this factor and the standard deviation
+            of the mean success rate: mean - (stdev * success_rate_stdev_factor).
+            This factor is divided by a thousand to get a double. That is, if the
+            desired factor is 1.9, the runtime value should be 1900. Defaults to 1900.'
+          returned: success
+          type: int
     protocol:
       description:
       - The protocol this RegionBackendService uses to communicate with backends.
@@ -300,6 +603,29 @@ resources:
         Default is 30 seconds. Valid range is [1, 86400].
       returned: success
       type: int
+    logConfig:
+      description:
+      - This field denotes the logging options for the load balancer traffic served
+        by this backend service.
+      - If logging is enabled, logs will be exported to Stackdriver.
+      returned: success
+      type: complex
+      contains:
+        enable:
+          description:
+          - Whether to enable logging for the load balancer traffic served by this
+            backend service.
+          returned: success
+          type: bool
+        sampleRate:
+          description:
+          - This field can only be specified if logging is enabled for this backend
+            service. The value of the field must be in [0, 1]. This configures the
+            sampling rate of requests to the load balancer where 1.0 means all logged
+            requests are reported and 0.0 means no logged requests are reported.
+          - The default value is 1.0.
+          returned: success
+          type: str
     network:
       description:
       - The URL of the network to which this backend service belongs.
