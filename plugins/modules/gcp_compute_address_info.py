7f65d2b8c110c917a555a1a899582e27bf8691dd
The Magician <magic-modules@google.com>
2021-05-14 13:23:55 -0700

    IPsec-encrypted Interconnect (#4777) (#416)
    * IPsec over interconnect feature
    
    * Removing test from other branch
    
    * Mark several encryption fields as input-only
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- plugins/modules/gcp_compute_address_info.py
+++ plugins/modules/gcp_compute_address_info.py
@@ -158,12 +158,16 @@ resources:
       type: str
     purpose:
       description:
-      - 'The purpose of this resource. Possible values include: * GCE_ENDPOINT for
-        addresses that are used by VM instances, alias IP ranges, internal load balancers,
-        and similar resources.'
+      - 'The purpose of this resource, which can be one of the following values: *
+        GCE_ENDPOINT for addresses that are used by VM instances, alias IP ranges,
+        internal load balancers, and similar resources.'
       - "* SHARED_LOADBALANCER_VIP for an address that can be used by multiple internal
         load balancers."
       - "* VPC_PEERING for addresses that are reserved for VPC peer networks."
+      - "* IPSEC_INTERCONNECT (Beta only) for addresses created from a private IP
+        range that are reserved for a VLAN attachment in an IPsec-encrypted Cloud
+        Interconnect configuration. These addresses are regional resources."
+      - This should only be set when using an Internal address.
       returned: success
       type: str
     networkTier:
