db55e4afb72f00e4ea5152a1e2de55659fb4ecd3
Tze <tze@aiyor.net>
2023-07-08 20:25:15 +1000

    nit: change author info to gitlab username

--- plugins/modules/gcp_secretmanager_info.py
+++ plugins/modules/gcp_secretmanager_info.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2023, Tze L. <tze@aiyor.net>
+# Copyright (C) 2023, Tze L. (gitlab.com/tze)
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import absolute_import, division, print_function
 import os
