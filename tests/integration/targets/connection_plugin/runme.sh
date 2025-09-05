5745bdaac2d36f380c77aab492ac5f73754e98b2
Jorge Gallegos <jgallego@redhat.com>
2025-09-04 19:28:23 -0700

    Integration test for new IAP connection plugin
    1. creates instances with a custom ssh keypair
    2. change the connection plugin method and perform basic checks
    3. cleanup

---
+++ tests/integration/targets/connection_plugin/runme.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# test infra
+ansible-playbook playbooks/setup.yml "$@"
+
+export ANSIBLE_INVENTORY=test.gcp_compute.yml
+
+ansible-inventory --graph
+
+RC=0
+# we want to run teardown regardless of playbook exit status, so catch the
+# exit code of ansible-playbook manually
+set +e
+ansible-playbook -vvvvv playbooks/test.yml "$@"
+RC=$?
+set -e
+
+unset ANSIBLE_INVENTORY
+
+# delete test infra
+ansible-playbook playbooks/teardown.yml "$@"
+
+exit $RC
