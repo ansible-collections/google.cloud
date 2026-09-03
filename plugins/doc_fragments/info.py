cc0c6e1f288c67ec39a6b83d8f0b7934d6e24a34
Jorge Gallegos <jgallego@redhat.com>
2026-09-03 16:14:29 -0700

    A simple doc_fragment plugin for info modules

---
+++ plugins/doc_fragments/info.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2026, Google Inc.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+
+class ModuleDocFragment(object):
+    # GCP info doc fragment.
+    DOCUMENTATION = r"""
+    options:
+        filters:
+            description:
+              - A list of filter expression strings used to filter the resources returned by the API.
+              - Each string is a filter expression (e.g. C(some_field = "SOME_VALUE")).
+              - Multiple expressions are combined with a logical AND.
+              - Refer to the filter topic documentation U(https://cloud.google.com/sdk/gcloud/reference/topic/filters).
+              - Refer to the IAP-160 filter syntax documentation U(https://google.aip.dev/160).
+            type: list
+            elements: str
+    """
