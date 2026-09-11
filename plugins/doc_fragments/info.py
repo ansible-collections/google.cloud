9758e8f34a06069530fbd765dff8cb4e9dc6aaea
Jorge Gallegos <jgallego@redhat.com>
2026-09-10 19:49:08 -0700

    Fix typo

--- plugins/doc_fragments/info.py
+++ plugins/doc_fragments/info.py
@@ -14,7 +14,7 @@ class ModuleDocFragment(object):
               - Each string is a filter expression (e.g. C(some_field = "SOME_VALUE")).
               - Multiple expressions are combined with a logical AND.
               - Refer to the filter topic documentation U(https://cloud.google.com/sdk/gcloud/reference/topic/filters).
-              - Refer to the IAP-160 filter syntax documentation U(https://google.aip.dev/160).
+              - Refer to the AIP-160 filter syntax documentation U(https://google.aip.dev/160).
             type: list
             elements: str
     """
